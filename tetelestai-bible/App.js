dab6810540f714d6a55816122af72854de107b94
Heitor da Rocha Liberal Neto <roseanaheitor@gmail.com>
2023-12-29 18:47:40 -0300

    criando splashscren

--- tetelestai-bible/App.js
+++ tetelestai-bible/App.js
@@ -1,8 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import * as SplashScreen from 'expo-splash-screen';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    async function prepare() {
+      try {
+        // keeps the splash screen visible while assets are cached
+        await SplashScreen.preventAutoHideAsync();
+
+        // pre-load/cache assets: images, fonts, and videos
+        await func.loadAssetsAsync();
+      } catch (e) {
+        // console.warn(e);
+      } finally {
+        // loading is complete
+        setIsLoading(false);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  React.useEffect(() => {
+    // when loading is complete
+    if (isLoading === false) {
+      // hide splash function
+      const hideSplash = async () => SplashScreen.hideAsync();
+
+      // hide splash screen to show app
+      hideSplash();
+    }
+  }, [isLoading]);
+
+  if (isLoading) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.openscreen}>TETELESTAI BIBLE</Text>
