15b21f91ce273f4ddd94c654ce9bba62f6f6a7d0
Heitor da Rocha Liberal Neto <roseanaheitor@gmail.com>
2024-01-03 16:04:02 -0300

    criando arquivos e ícones splash screen.

--- bygrace/App.js
+++ bygrace/App.js
@@ -1,21 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Styles } from './src/style';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.openscreen}>BY GRACE</Text>
+      <Text style={styles.subtitleopenscreen}>BIBLE</Text>
       <StatusBar style="auto" />
     </View>
-  );
-}
+  )
+  }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+container: {
+  flex: 1,
+  backgroundColor: '#000',
+  alignItems: 'center',
+  justifyContent: 'center',
+},
+
+openscreen: {
+  fontSize: 30,
+  color: "white",
+  fontWeight: "bold",
+  textAlign: 'center',
+},
+
+subtitleopenscreen: {
+  fontSize: 18,
+  color: "white",
+  textAlign: 'center',
+}
+
+}
+);
---
+++ bygrace/src/style.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#000',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    
+    openscreen: {
+      fontSize: 30,
+      color: "white",
+      fontWeight: "bold",
+      textAlign: 'center',
+    },
+    
+    subtitleopenscreen: {
+      fontSize: 18,
+      color: "white",
+      textAlign: 'center',
+    }
+})
