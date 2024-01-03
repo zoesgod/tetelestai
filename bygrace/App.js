17a4a7bfdf74f007aa28620e0bccccba05766544
Heitor da Rocha Liberal Neto <roseanaheitor@gmail.com>
2024-01-03 20:49:48 -0300

    criando arquivos e ícones splash screen.

--- bygrace/App.js
+++ bygrace/App.js
@@ -1,39 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import * as SplashScreen from 'expo-splash-screen';
-import React, { useCallback, useEffect, useState } from 'react';
-import { Styles } from './src/style';
-import { StyleSheet, Text, View } from 'react-native';
+import { styles } from './src/style';
+import { Text, View } from 'react-native';
+
+//SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.openscreen}>BY GRACE</Text>
-      <Text style={styles.subtitleopenscreen}>BIBLE</Text>
+      <Text style={styles.openscreen}>WELCOME TO</Text>
+      <Text style={styles.subtitleopenscreen}>BY GRACE BIBLE</Text>
       <StatusBar style="auto" />
     </View>
   )
-  }
+  };
 
-const styles = StyleSheet.create({
-container: {
-  flex: 1,
-  backgroundColor: '#000',
-  alignItems: 'center',
-  justifyContent: 'center',
-},
-
-openscreen: {
-  fontSize: 30,
-  color: "white",
-  fontWeight: "bold",
-  textAlign: 'center',
-},
-
-subtitleopenscreen: {
-  fontSize: 18,
-  color: "white",
-  textAlign: 'center',
-}
-
-}
-);
--- bygrace/src/style.js
+++ bygrace/src/style.js
@@ -3,21 +3,21 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#000',
+      backgroundColor: '#600000',
       alignItems: 'center',
       justifyContent: 'center',
     },
     
     openscreen: {
-      fontSize: 30,
+      fontSize: 20,
       color: "white",
-      fontWeight: "bold",
       textAlign: 'center',
     },
     
     subtitleopenscreen: {
-      fontSize: 18,
+      fontSize: 30,
       color: "white",
+      fontWeight: "bold",
       textAlign: 'center',
     }
 })
