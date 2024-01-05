7d20a74ca55bf725b434704708b582db64de720c
Heitor da Rocha Liberal Neto <roseanaheitor@gmail.com>
2024-01-04 23:31:32 -0300

    corrigindo aplicação.

---
+++ bible/App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.mainscreen}>ARCO EDUCAÇÃO</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  mainscreen: {
+    color: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+  }
+});
