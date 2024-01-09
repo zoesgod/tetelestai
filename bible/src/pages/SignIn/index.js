d746c13da27ee383567a2cca68ea8077f3d90ea7
Heitor da Rocha Liberal Neto <roseanaheitor@gmail.com>
2024-01-09 00:19:26 -0300

    Criando multiple languages.

--- bible/src/pages/SignIn/index.js
+++ bible/src/pages/SignIn/index.js
@@ -1,13 +1,29 @@
+import { useState} from 'react'
 import React from "react";
 import { View, Text, StyleSheet, TextInput, TouchableOpacity}  from "react-native";
 import * as Animatable from 'react-native-animatable'
+import '../../utils/i18n'
+import { useTranslation } from 'react-i18next';
 
 export default function SignIn() {
+ const [currentLanguage, setCurrentLanguage] = useState("pt")
+
+ const {t, i18n} = useTranslation();
+ 
+ const changeLanguage = (value) => {
+  i18n.changeLanguage(value)
+  .then (() => {
+    setCurrentLanguage(value)
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+ }
     return (
       <View style={styles.container}>
         <Animatable.View animation="fadeInLeft" delay={500} style={styles.containerHeader}>
-          <Text style={styles.message}>Welcome to BY GRACE</Text>
-          <Text style={styles.message}>The Word of God in your Heart</Text>
+          <Text style={styles.message}>{t('Welcome to BY GRACE')}</Text>
+          <Text style={styles.message}>{t('The Word of God in your Heart!')}</Text>
         </Animatable.View>
 
         <View style={styles.containerLogo}>
@@ -18,27 +34,56 @@ export default function SignIn() {
           resizeMode="contain"
         />
         </View>
+        
+        <View style={styles.languages}>
+          
+          <TouchableOpacity
+          onPress={() => changeLanguage('pt')}
+          style={[
+            styles.langButton,{
+            borderColor: currentLanguage === 'pt' ? "#FFF": 'transparent',  
+            }]
+          }
+          >
+            <Text style={styles.textButton}>{t('Portuguese')}</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+          onPress={() => changeLanguage('en')}
+          style={[
+            styles.langButton,{
+            borderColor: currentLanguage === 'en' ? "#FFF": 'transparent',  
+            }]
+          }
+          >
+            <Text style={styles.textButton}>{t('English')}</Text>
+          </TouchableOpacity>
+        </View>
 
         <Animatable.View animation="fadeInUp" style={styles.containerForm}>
-        <Text style={styles.title}>E-mail</Text>
+        <Text style={styles.title}>{t('Email')}</Text>
         <TextInput
           placeholder="E-mail"
           style={styles.input}
         />
         
-        <Text style={styles.title}>Password</Text>
+        <Text style={styles.title}>{t('Password')}</Text>
         <TextInput
           placeholder="Password"
           style={styles.input}
         />
 
         <TouchableOpacity style={styles.button}>
-          <Text style={styles.textButton}>Connect</Text>
+          <Text style={styles.textButton}>{t('Connect')}</Text>
         </TouchableOpacity>
         
         <TouchableOpacity style={styles.register}>
-          <Text style={styles.textRegister}>Don´t have an Account? Sign Up</Text>
+          <Text style={styles.textRegister}>{t('Not a member? Sign Up')}</Text>
         </TouchableOpacity>
+
+        <TouchableOpacity style={styles.forgot}>
+          <Text style={styles.textForgot}>{t('Forgot Password? Click Here.')}</Text>
+        </TouchableOpacity>  
 
         </Animatable.View>
       </View>
@@ -51,15 +96,46 @@ const styles = StyleSheet.create ({
     backgroundColor: "#1B243D",
   },
 
+  langButton:{
+    borderWidth: 1,
+    padding: 4,
+    borderRadius: 4,
+    marginRight: 4,
+    marginLeft: 4,
+  },
+  
+  languages:{
+    flexDirection: 'row',
+    position: 'absolute',
+    borderRadius: 12,
+    paddingVertical: 25,
+    width: "100%",
+    marginTop: 14,
+    alignSelf: "center",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
   containerHeader:{
-    marginTop: "24%",
+    marginTop: "20%",
     alignItems: "center",
   },
 
   message:{
+    marginTop: "3%",
     fontSize: 20,
     color: "#FFF",
     fontWeight: "bold",
+  },
+
+  containerLogo:{
+    alignItems: "center",
+  },
+  
+  textButton:{
+    marginRight: 4,
+    marginLeft: 4,
+    color: '#FFF',
   },
 
   containerForm:{
@@ -81,14 +157,14 @@ const styles = StyleSheet.create ({
     borderBottomWidth: 1,
     height: 40,
     marginBottom: 12,
-    fontSize: 16 
+    fontSize: 14 
   },
 
   button:{
     backgroundColor: "#1B243D",
     borderRadius: 12,
     paddingVertical: 12,
-    width: "60%",
+    width: "100%",
     marginTop: 14,
     alignSelf: "center",
     justifyContent: "center",
@@ -100,20 +176,22 @@ const styles = StyleSheet.create ({
     elevation: 5,
   },
   
-    textButton:{
+  textButton:{
     color: "#FFF",
     fontSize: 15,
     fontWeight: "bold"
   },
   
   register:{
-  marginTop: 14,
-  alignSelf: "center",
-  color: "#010101"
+    marginTop: 14,
+    alignSelf: "center",
+    color: "#010101",
   },
 
-  textRegister:{
+  textForgot:{
+    marginTop: 5,
+    alignSelf: "center",
+    color: "#010101",
+    }
 
-  }
-  
 });
--- bible/src/routes/index.js
+++ bible/src/routes/index.js
@@ -1,15 +1,11 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-//import Welcome from '../pages/Welcome'
 import SignIn from '../pages/SignIn'
 
 const Stack = createNativeStackNavigator();
 
 export default function Routes(){
     return(
-        <Stack.Navigator initialRouteName="Welcome">
-            {/* <Stack.Screen
-                name="Welcome"
-                component={Welcome} options={{headerShown: false}} /> */}
+        <Stack.Navigator initialRouteName="SignIn">
             <Stack.Screen
                 name="SignIn"
                 component={SignIn} options={{headerShown: false}} />
